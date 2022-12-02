e9361a27235f87f7f7fca2c9abf12fa74bcf5539
Jamie Daly <jamie.daly@infinity.co>
2022-12-02 10:31:06 +0000

    Update file 1

--- xmas-2022-challenges/xmas-01/xmas_01.js
+++ xmas-2022-challenges/xmas-01/xmas_01.js
@@ -1,27 +1,8 @@
-// PART 1
+// PART 1 & 2
 //---------------------------------
 
 import { inputs } from './inputs.js'
 
-let highestCalorieElf = 0;
-let currentElf = 0;
-
-for (let i = 0; i < testArray.length; i++) {
-    if (testArray[i] !== 0) {
-        currentElf =+ testArray[i]
-
-    } else {
-        if (highestCalorieElf < currentElf) {
-            highestCalorieElf = currentElf
-            currentElf = 0
-            console.log(highestCalorieElf)
-        }
-    }
-}
-
-
-
-/*
 let total = 0
 let highestNumber = 0
 let highNumberArray = []
@@ -40,5 +21,3 @@ inputs.forEach( newNumber => {
 console.log(highNumberArray.sort())
 console.log(highNumberArray[highNumberArray.length - 1])
 console.log(highNumberArray[highNumberArray.length - 1] + highNumberArray[highNumberArray.length - 2] + highNumberArray[highNumberArray.length - 3])
-
-*/
