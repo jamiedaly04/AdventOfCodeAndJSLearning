a789e5ce23eb760376c7739ba0db76ffdd7f6598
Jamie Daly <jamie.daly@msite.com>
2024-12-06 08:29:27 +0000

    Complete Day 1

--- xmas-2024-challenges/xmas-01/solution.ts
+++ xmas-2024-challenges/xmas-01/solution.ts
@@ -1,18 +1,19 @@
 import { array1, array2 } from './inputs.ts';
 
-// PART 1
-//---------------------------------
-
-function getNumberDifference(a: number, b: number) {
-  return Math.abs(a - b);
-}
+//Test Data Example
+//const leftList = [3, 4, 2, 1, 3, 3];
+//const rightList = [4, 3, 5, 3, 9, 3];
 
 const leftList = array1;
 const rightList = array2;
-/* Test Data Example
-const leftList = [3, 4, 2, 1, 3, 3];
-const rightList = [4, 3, 5, 3, 9, 3];
-*/
+
+// PART 1
+//---------------------------------
+
+/* function getNumberDifference(a: number, b: number) {
+  return Math.abs(a - b);
+}
+
 const distanceArray = new Array<number>();
 let totalDistance = 0;
 
@@ -35,7 +36,36 @@ for (let i = 0; i < distanceArray.length; i++) {
   totalDistance += distanceArray[i];
 }
 
-console.log(`Total Distance: ${totalDistance}`);
+console.log(`Total Distance: ${totalDistance}`); */
 
 // PART 2
 //---------------------------------
+
+const occurenceArray = new Array<number>();
+const similarityArray = new Array<number>();
+
+// Loop through left array value for each right array value
+// If it finds value, add to counter
+for (let i = 0; i < leftList.length; i++) {
+  let count = 0;
+
+  for (let x = 0; x < rightList.length; x++) {
+    if (leftList[i] === rightList[x]) {
+      count = count + 1;
+    }
+  }
+
+  // Build occurence array from counter
+  occurenceArray.push(count);
+
+  // Build similarity array
+  similarityArray.push(leftList[i] * occurenceArray[i]);
+}
+
+// Add together all values in similarity array
+const totalSimilarityScore = similarityArray.reduce(
+  (partialSum, a) => partialSum + a,
+  0
+);
+
+console.log(totalSimilarityScore);
