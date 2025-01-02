446b0131080bbd77f7310ea440d3db1cf0ca29ac
Jamie Daly <jamie.daly@msite.com>
2025-01-02 14:08:18 +0000

    Day 6 Part 2

--- xmas-2024-challenges/xmas-06/solution.ts
+++ xmas-2024-challenges/xmas-06/solution.ts
@@ -1,9 +1,9 @@
 import { importedPuzzleInput } from './inputs.ts';
 
-//let puzzleInput = [...importedPuzzleInput];
+let puzzleInput = [...importedPuzzleInput];
 
 // Test Data Example
-let puzzleInput: string[] = [
+/* let puzzleInput: string[] = [
   '....#.....',
   '.........#',
   '..........',
@@ -15,7 +15,7 @@ let puzzleInput: string[] = [
   '#.........',
   '......#...',
 ];
-
+ */
 //---------------------------------
 // PART 1
 
@@ -151,14 +151,16 @@ console.log(`Maximum X Value: ${maximumXValue}`);
 console.log(`Maximum Y Value: ${maximumYValue}`);
 // Get current guard position
 [guardPosition, guardDirection] = getGuardPositionAndDirection(puzzleInput);
+let startingGuardPosition: { x: number | null; y: number | null };
+startingGuardPosition = guardPosition;
 
 // Get position in front of guard
 positionInFront = getPositionInFrontOfGuard(guardDirection, guardPosition);
 
-console.log(`Guard Position: ${JSON.stringify(guardPosition)}`);
-console.log(`Position In Front: ${JSON.stringify(positionInFront)}`);
-console.log(`Guard Direction: ${guardDirection}`);
-console.log(`Starting Puzzle Input: `);
+//console.log(`Guard Position: ${JSON.stringify(guardPosition)}`);
+//console.log(`Position In Front: ${JSON.stringify(positionInFront)}`);
+//console.log(`Guard Direction: ${guardDirection}`);
+//console.log(`Starting Puzzle Input: `);
 
 // Uncomment this to see grid at start
 for (let x = 0; x < puzzleInput.length; x++) {
@@ -166,17 +168,17 @@ for (let x = 0; x < puzzleInput.length; x++) {
   console.log(element);
 }
 
-console.log('\n-------------------------\n');
+//console.log('\n-------------------------\n');
 
 while (guardPosition.x !== 0 && guardPosition.y !== 0 && guardPosition.x !== maximumXValue && guardPosition.y !== maximumYValue) {
   if (puzzleInput[positionInFront.y ?? 0][positionInFront.x ?? 0] === '#') {
-    console.log('DIRECTION CHANGED!');
+    //console.log('DIRECTION CHANGED!');
     changeGuardDirection(guardDirection);
   } else if (
     puzzleInput[positionInFront.y ?? 0][positionInFront.x ?? 0] === '.' ||
     puzzleInput[positionInFront.y ?? 0][positionInFront.x ?? 0] === 'X'
   ) {
-    console.log('GUARD MOVED!');
+    //console.log('GUARD MOVED!');
     moveGuard(guardPosition, positionInFront, guardDirection);
   }
 
@@ -186,17 +188,17 @@ while (guardPosition.x !== 0 && guardPosition.y !== 0 && guardPosition.x !== max
   // Get position in front of guard
   positionInFront = getPositionInFrontOfGuard(guardDirection, guardPosition);
 
-  console.log(`Guard Position: ${JSON.stringify(guardPosition)}`);
-  console.log(`Position In Front: ${JSON.stringify(positionInFront)}`);
-  console.log(`Guard Direction: ${guardDirection}\n`);
+  //console.log(`Guard Position: ${JSON.stringify(guardPosition)}`);
+  //console.log(`Position In Front: ${JSON.stringify(positionInFront)}`);
+  //console.log(`Guard Direction: ${guardDirection}\n`);
 
   // Uncomment this to see grid at each step
-  for (let x = 0; x < puzzleInput.length; x++) {
+  /*   for (let x = 0; x < puzzleInput.length; x++) {
     const element = puzzleInput[x];
     console.log(element);
-  }
+  } */
 
-  console.log('\n-------------------------\n');
+  //console.log('\n-------------------------\n');
 }
 
 // One last guard movement, to show him leaving grid
@@ -206,7 +208,7 @@ moveGuard(guardPosition, positionInFront, guardDirection);
 let answer = 0;
 
 // Final Grid
-console.log('Final Grid:\n');
+/* console.log('Final Grid:\n');
 
 for (let x = 0; x < puzzleInput.length; x++) {
   const element = puzzleInput[x];
@@ -214,7 +216,7 @@ for (let x = 0; x < puzzleInput.length; x++) {
   console.log(element);
 }
 
-console.log('\n-------------------------\n');
+console.log('\n-------------------------\n'); */
 
 for (let i = 0; i < puzzleInput.length; i++) {
   for (let x = 0; x < puzzleInput[i].length; x++) {
@@ -228,7 +230,158 @@ for (let i = 0; i < puzzleInput.length; i++) {
 
 console.log(`Final Answer: ${answer}`);
 
-// PART 2 - CHECK ALL X LOCATIONS FROM PART 1
-//  AND ADD OBSTRUCTION, THEN LOOP THROUGH THAT MAP TO AND SEE IF YOU GET STUCK IN A LOOP
-// STUCK IN A LOOP = LOCATION ALREADY VISITED (X, Y, DIRECTION) SAME AS BEFORE
-// JUST STORE VISITED LOCATIONS
+//---------------------------------
+// PART 2
+
+let xPositionArray: { x: number | null; y: number | null }[] = [];
+
+console.log('\n-------------------------\n');
+
+// Build array of all 'X' positions where obstacle can go
+for (let i = 0; i < puzzleInput.length; i++) {
+  for (let x = 0; x < puzzleInput[i].length; x++) {
+    const element = puzzleInput[i][x];
+    if (element === 'X') {
+      xPositionArray.push({ x: x, y: i });
+    }
+  }
+}
+
+// Remove starting position
+const startPos = xPositionArray.findIndex((x) => x.x === startingGuardPosition.x && x.y === startingGuardPosition.y);
+xPositionArray.splice(startPos, 1);
+console.log(xPositionArray);
+
+let puzzleInputPart2: string[] = [...importedPuzzleInput];
+
+// PART 2 Start
+let part2Answer = 0;
+
+xPositionArray.forEach((obstacle, index) => {
+  let indexerThingy = 0;
+  let modifiedPuzzle: string[] = puzzleInputPart2;
+  modifiedPuzzle = puzzleInputPart2.map((row, yindex) => {
+    return row
+      .split('')
+      .map((char, xIndex) => {
+        if (yindex === obstacle.y && xIndex === obstacle.x) {
+          return 'O';
+        } else {
+          return char;
+        }
+      })
+      .join('');
+  });
+
+  // THEN LOOP THROUGH THAT MAP TO AND SEE IF GUARD GETS STUCK IN A LOOP
+  // STUCK IN A LOOP = LOCATION ALREADY VISITED (X, Y, DIRECTION) SAME AS BEFORE
+  // JUST STORE VISITED LOCATIONS
+  /*   for (let x = 0; x < modifiedPuzzle.length; x++) {
+    console.log(modifiedPuzzle[x]);
+  } */
+
+  // Get current guard position
+  [guardPosition, guardDirection] = getGuardPositionAndDirection(puzzleInputPart2);
+
+  // Get position in front of guard
+  positionInFront = getPositionInFrontOfGuard(guardDirection, guardPosition);
+
+  while (
+    guardPosition.x !== 0 &&
+    guardPosition.y !== 0 &&
+    guardPosition.x !== maximumXValue &&
+    guardPosition.y !== maximumYValue &&
+    indexerThingy < 3000
+  ) {
+    if (
+      modifiedPuzzle[positionInFront.y ?? 0][positionInFront.x ?? 0] === '#' ||
+      modifiedPuzzle[positionInFront.y ?? 0][positionInFront.x ?? 0] === 'O'
+    ) {
+      console.log('DIRECTION CHANGED!');
+      let newGuardDirection: string = '';
+
+      switch (guardDirection) {
+        case '^':
+          newGuardDirection = '>';
+          break;
+        case '>':
+          newGuardDirection = 'v';
+          break;
+        case 'v':
+          newGuardDirection = '<';
+          break;
+        case '<':
+          newGuardDirection = '^';
+          break;
+        default:
+          console.log('Guard Direction Change Error');
+          break;
+      }
+
+      modifiedPuzzle = modifiedPuzzle.map((row) => {
+        return row
+          .split('')
+          .map((char) => {
+            if (char === guardDirection) {
+              return newGuardDirection;
+            } else {
+              return char;
+            }
+          })
+          .join('');
+      });
+    } else if (
+      modifiedPuzzle[positionInFront.y ?? 0][positionInFront.x ?? 0] === '.' ||
+      modifiedPuzzle[positionInFront.y ?? 0][positionInFront.x ?? 0] === 'X'
+    ) {
+      console.log('GUARD MOVED!');
+      // Update space left behind to be an 'X'
+      modifiedPuzzle = modifiedPuzzle.map((row, y) => {
+        return row
+          .split('')
+          .map((char, x) => {
+            if (x === guardPosition.x && y === guardPosition.y) {
+              return 'X';
+            } else if (x === positionInFront.x && y === positionInFront.y) {
+              return guardDirection;
+            } else {
+              return char;
+            }
+          })
+          .join('');
+      });
+
+      // Move guard forward one space
+      guardPosition = positionInFront;
+    }
+
+    // Get current guard position
+    [guardPosition, guardDirection] = getGuardPositionAndDirection(modifiedPuzzle);
+
+    // Get position in front of guard
+    positionInFront = getPositionInFrontOfGuard(guardDirection, guardPosition);
+
+    console.log(`Guard Position: ${JSON.stringify(guardPosition)}`);
+    console.log(`Position In Front: ${JSON.stringify(positionInFront)}`);
+    console.log(`Guard Direction: ${guardDirection}\n`);
+
+    // Uncomment this to see grid at each step
+    /*     for (let x = 0; x < modifiedPuzzle.length; x++) {
+      const element = modifiedPuzzle[x];
+      console.log(element);
+    } */
+
+    console.log('\n-------------------------\n');
+    console.log(indexerThingy);
+    indexerThingy++;
+
+    if (indexerThingy === 2998) {
+      part2Answer += 1;
+    }
+  }
+  console.log('\n-------------------------\n');
+  console.log('\n-------------------------\n');
+  console.log('\n-------------------------\n');
+});
+
+console.log(part2Answer);
